4f78662003fd7e8c7855cd4da987acfcda8ca9e7
AuskinImmanuel <auskinimmanuel.24cs@licet.ac.in>
2023-03-03 14:35:35 +0530

    done for the day

--- CAT questions/script_q2.js
+++ CAT questions/script_q2.js
@@ -1,5 +1,5 @@
 var req = new XMLHttpRequest()
-url = "data.txt"
+url = "file: data.txt"
 
 req.open("GET","data.txt",true)
 
@@ -7,9 +7,31 @@ req.send()
 
 req.onload = function(){
     table_data = Object.values(JSON.parse(this.response));
-    table =[]
-    for(var i=0;i<table_data.length;i++){
-        table.push(table_data[i].name)
+    table_name =[]
+    table_roll =[]
+    table_marks =[]
+
+    for(var i =0;i<table_data.length;i++){
+        var output=""
+        output+="<table border=1><tr><th>Name</th></tr>"
+
+        document.getElementById("out").innerHTML=output
+        
+        table_name.push(table_data[i].name)
+        table_roll.push(table_data[i].roll)
+        table_marks.push(table_data[i].mark)
+
+        names = document.createElement("tr")
+        names.innerHTML = table_data[i].name
+        document.body.appendChild(names)    
+
+        header = document.createElement("tr")
+        header.innerHTML = table_data[i].roll
+        document.body.appendChild(header) 
+
+        header = document.createElement("tr")
+        header.innerHTML = table_data[i].mark
+        document.body.appendChild(header) 
     }
-    alert(table)
+    // alert(table)
 }
